d71df992e873480667213e1320e1cc4a8f8d45a4
cultofmetatron <cultofmetatron@aumlogic.com>
2013-05-27 18:55:29 -0700

    updated generator

--- lib/generators/thorax/install/install_generator.rb
+++ lib/generators/thorax/install/install_generator.rb
@@ -1,18 +1,14 @@
-require 'generators/backbone/resource_helpers'
+require 'generators/thorax/resource_helpers'
 
-Module Thorax
-  Module Generators
+module Thorax
+  module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Backbone::Generators::ResourceHelpers
+      include Thorax::Generators::ResourceHelpers
 
       source_root File.expand_path("../templates", __FILE__)
-      desc "This generator installs backbone.js, handlebars, thorax and thorax-mobile " +
-           " in app/assets/javascripts/thorax"
+      desc "This generator installs backbone.js, handlebars, thorax and thorax-mobile in app/assets/javascripts/thorax"
 
-      class_option :skip_git, :type    => :boolean,
-                              :aliases => "-G",
-                              :default => false,
-                              :desc    => "Skip Git ignores and keeps"
+      class_option :skip_git,  :type    => :boolean, :aliases => "-G", :default => false, :desc => "Skip Git ignores and keeps"
 
       def inject_thorax
         inject_into_file "app/assets/javascripts/application.js", :before => "//=require_tree" do
@@ -25,21 +21,19 @@ Module Thorax
       end
 
       def create_dir_layout
-        %w{routers models views templates}.each do |dir|
+        ['routers', 'models', 'views', 'templates'].each do |dir|
           empty_directory "app/assets/javascripts/thorax/#{dir}"
-          create_file "app/assets/javascripts/thorax/#{dir}/.gitkeep" unless options[:skip_git]
+          create_file("app/assets/javascripts/thorax/#{dir}/.gitkeep") unless options[:skip_git]
         end
       end
 
       def create_app_file
-        template "app.coffee", "app/assets/javascripts/thorax/#{application_name.underscore}.js/coffee"
+        template("app.coffee", "app/assets/javascripts/thorax/#{application_name.underscore}.js/coffee")
       end
 
     end
   end
 end
-
-
 
 
 
